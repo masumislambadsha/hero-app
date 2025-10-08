360df5582dc080947ee13c43d01c6e532e783ac1
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-10-08 16:03:36 +0600

    updated slice

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ import useAllAppsData from "../Hooks/useAllAppsData";
 const HomePage = () => {
   const { datas, loading } = useAllAppsData();
 
-  const homeData = datas.slice(0, 8);
+  const homeData = datas.slice(16, 24);
 
   if (loading) {
     return <LoadingSpinner></LoadingSpinner>;
