4ef17732f2622c86f15bfc64ad0a8098096f0115
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-10-08 14:42:13 +0600

    updated app details

--- src/Pages/InstalledApps.jsx
+++ src/Pages/InstalledApps.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const InstalledApps = () => {
+  const installedApps = JSON.parse(localStorage.getItem("installed"));
+  console.log(installedApps);
+  installedApps.map((installedApp) => {
+    console.log(installedApp.title);
+
+  });
+
   return (
-    <div>
-        isntalleskdjfaklsjdfh;laksjdfhlkasdjfh
+    <div className="container mx-auto">
+      {installedApps.map((installedApp) => {
+        return(
+             <h1>{installedApp.title}</h1>
+        )
+
+      })}
     </div>
   );
 };
